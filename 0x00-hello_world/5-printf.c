3274a185bd6225e5223ebb9e5a26a32f60cf4dc6
CMSteam-hm <saed.leo@gmail.com>
2025-02-26 05:52:01 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point of the program
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: Prints "with proper"
  * using the printf function.
  * Return: Always 0 (Success)
  */
