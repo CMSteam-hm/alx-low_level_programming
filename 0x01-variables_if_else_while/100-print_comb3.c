1f9bbe66e15079876c53019aba4127add7aa2ce8
CMSteam-hm <saed.leo@gmail.com>
2025-02-27 07:55:47 -0800

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers
+ * main - Prints all possible different combinations of two digits
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-int digit = '0';
+int s_digi;
+int d_digi;
 
-while (digit <= '9')
+for (d_digi = '0'; d_digi <= '9'; d_digi++) /*++ double digits*/
 {
-putchar(digit);
-if (digit != '9')
+for (s_digi = (d_digi + 1); s_digi <= '9'; s_digi++) /*++ single digits*/
+{
+putchar(d_digi);
+putchar(s_digi);
+
+if (d_digi != '8' || s_digi != '9') /*seperates values with commas*/
 {
 putchar(',');
 putchar(' ');
 }
-digit++;
+}
 }
 putchar('\n');
 
 return (0);
+
 }
