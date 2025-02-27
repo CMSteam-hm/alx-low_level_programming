e684bc966f13d0a316b0ee5872fd430a277e560e
CMSteam-hm <saed.leo@gmail.com>
2025-02-27 03:36:22 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * main - Entry point
- *
- * Description: Generates a random number and determines if it is
- * positive, negative, or zero.
+/**
+ * main - Generates a random number and determines if it is positive,
+ *        negative, or zero.
  *
  * Return: Always 0 (Success)
  */
