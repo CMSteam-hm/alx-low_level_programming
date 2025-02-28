eb26699d14a01952228a2b5623a641597e4d80e5
CMSteam-hm <saed.leo@gmail.com>
2025-02-28 02:20:59 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints '_putchar' followed by a newline
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char x[] = "_putchar";
+int y;
+
+for (y = 0; x[y] != '\0'; y++)
+{
+_putchar(x[y]);
+}
+
+_putchar('\n');
+return (0);
+}
