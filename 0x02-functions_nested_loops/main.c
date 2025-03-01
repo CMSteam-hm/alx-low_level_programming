bab1aa643ddca091c5ce75690cd6d6803de99ed9
CMSteam-hm <saed.leo@gmail.com>
2025-02-28 17:21:01 -0800

    jack bauer

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,24 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Tests the print_last_digit function
+ * main - Entry point, calls jack_bauer function
  *
  * Return: 0 on success
  */
 int main(void)
 {
-int r;
-
-r = print_last_digit(98);
-printf(": %d\n", r);
-
-r = print_last_digit(-1234);
-printf(": %d\n", r);
-
-r = print_last_digit(0);
-printf(": %d\n", r);
-
+jack_bauer();
 return (0);
 }
 
