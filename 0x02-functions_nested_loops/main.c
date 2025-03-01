cc12a34829d33af64f7cd1cada97a7044e6ee7c5
CMSteam-hm <saed.leo@gmail.com>
2025-02-28 18:03:55 -0800

    add

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,13 +1,44 @@
 #include "main.h"
 
 /**
- * main - Entry point, calls times_table function
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+
+if (n / 10)
+print_number(n / 10);
+
+_putchar((n % 10) + '0');
+}
+
+/**
+ * main - Entry point, tests the add function
  *
  * Return: 0 on success
  */
 int main(void)
 {
-times_table();
+int result;
+
+result = add(5, 7);
+print_number(result);
+_putchar('\n');
+
+result = add(-3, 8);
+print_number(result);
+_putchar('\n');
+
+result = add(0, 0);
+print_number(result);
+_putchar('\n');
+
 return (0);
 }
 
