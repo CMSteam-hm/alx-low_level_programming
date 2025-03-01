6daec55f1891753142fdfe62be043c2afc1cf18d
CMSteam-hm <saed.leo@gmail.com>
2025-02-28 18:42:44 -0800

    mult table

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point, calls print_to_98 function
+ * main - Entry point, calls times_table function
  *
  * Return: 0 on success
  */
 int main(void)
 {
-print_to_98(0); /* Test case: starting from 0 */
-print_to_98(98); /* Test case: already at 98 */
-print_to_98(111); /* Test case: descending order */
-print_to_98(-10); /* Test case: negative start */
-
+times_table();
 return (0);
 }
 
