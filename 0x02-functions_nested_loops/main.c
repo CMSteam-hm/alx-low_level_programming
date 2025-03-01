c3b39ddbc9c02573d8457335c098de23c17d2975
CMSteam-hm <saed.leo@gmail.com>
2025-02-28 18:32:46 -0800

    98

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,45 +1,17 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer using _putchar
- * @n: The integer to print
- */
-void print_number(int n)
-{
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-
-if (n / 10)
-print_number(n / 10);
-
-_putchar((n % 10) + '0');
-}
-
-/**
- * main - Entry point, tests the add function
+ * main - Entry point, calls print_to_98 function
  *
  * Return: 0 on success
  */
 int main(void)
 {
-int result;
-
-result = add(5, 7);
-print_number(result);
-_putchar('\n');
-
-result = add(-3, 8);
-print_number(result);
-_putchar('\n');
-
-result = add(0, 0);
-print_number(result);
-_putchar('\n');
+print_to_98(0); /* Test case: starting from 0 */
+print_to_98(98); /* Test case: already at 98 */
+print_to_98(111); /* Test case: descending order */
+print_to_98(-10); /* Test case: negative start */
 
 return (0);
 }
-
 
