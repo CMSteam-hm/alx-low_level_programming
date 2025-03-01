9b5e18ebfa68c1da1c048d43ea2696cb134526e7
CMSteam-hm <saed.leo@gmail.com>
2025-02-28 16:54:50 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,12 @@ int print_last_digit(int n)
 {
 int last_digit;
 
-last_digit = n % 10; // Get the last digit
+last_digit = n % 10; /* Get the last digit */
 
-if (last_digit < 0) // Ensure it's positive
+if (last_digit < 0) /* Ensure it's positive */
 last_digit *= -1;
 
-_putchar(last_digit + '0'); // Print the last digit
+_putchar(last_digit + '0'); /* Print the last digit */
 
 return (last_digit);
 }
