594f3585f0a58ad018d30c01ffc9d002be29a0bb
CMSteam-hm <saed.leo@gmail.com>
2025-03-01 07:34:05 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point, tests the print_fibonacci function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+print_fibonacci(); /* Example: printing Fibonacci sequence */
+_putchar('\n');
+return (0);
+}
+
