73ad4463a275a8af86d0e20293d8ed3f454768cd
CMSteam-hm <saed.leo@gmail.com>
2025-03-29 10:20:05 -0700

    jst

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,39 +1,19 @@
-/*
- * Command line, Task 3: Program that multiplies two numbers.
- * Program should print result of multiplication,
- * followed by a '\n'.
- * Assume two digits & result of the multiplication should be
- * stored in an integer.
- * Program should print "ERROR\n" if it does receives args
- * other than two.
- */
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * main - entry point
- * @argc: argument count, number of cmdln arguments passed.
- * @argv: argument vector, arrays of strings that contains
- * the cmdln arguments.
- * Return: zero '0' success
- */
 int main(int argc, char *argv[])
 {
-int x, y, z;
-
-/* accepts only two arguments */
 if (argc != 3)
 {
 printf("Error\n");
-return (1);
+return 1;
 }
 
-/* converts string digit args to int value */
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-/* multiplies two numbers */
-z = x * y;
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+int result = num1 * num2;
 
-/* prints the result */
-printf("%i\n", z);
-return (0);
+printf("%d\n", result);
+return 0;
 }
+
